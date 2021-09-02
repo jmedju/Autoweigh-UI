1ef0af4fa326966255022c832e952bef5586a9ce
mtkelly699 <matthew.kelly699@gmail.com>
2021-09-02 16:44:12 -0400

    Update Data_Output_Test.py

--- Data_Output_Test.py
+++ Data_Output_Test.py
@@ -17,25 +17,44 @@ for x in range(15):
 
 now = datetime.now() #Gets the date and time
 dt = now.strftime("%y_%m_%d Time_%Hh%Mm%Ss") #Formats dte and time for filename purposes
-
-traytxt = "AA"
-tray = traytxt #String that holds tray alphanumerics
-cupnumber = 1 #First cup
+traytxt1 = "AA" #Initial tray of the run. Temporary variable: should be replaced with traytxt from setup parameters
+tray1 = traytxt1 #String that holds tray alphanumerics
+tray2 = "AB" #These trays are just examples. Each one should have its own corresponding traytxt(number) derived from the setup parameters
+tray3 = "AC"
+tray4 = "AD"
+tray5 = "AE"
+cupnumber = 1 #Initial cup of the run. Temporary variable: Should be replaced with cupnumber from setup parameters
 cup = '{0:05d}'.format(cupnumber) #Formats cup number properly
-filename = tray + cup + "_" + dt + ".txt" #collates the information into the proper file name
-file = open(r"./Data_Testing/"+filename, "w") #Creates a file with the correct name
-
-file.write("***********************************\n")
-file.write("     Weight Data File\n")
+filename = tray1 + cup + "_" + dt + ".txt" #collates the information into the proper file name
+file = open(r"./Data_testing/"+filename, "w") #Creates a file with the correct name
+file.write("***********************************\n") #This and the next few lines create the header for the text file
+file.write("        Weight Data File\n")
 file.write("***********************************\n")
 file.write("\n")
-file.write("Sample, Weight \n")
+file.write("Sample, Weight\n")
 file.write("\n")
-
-for x in range(15):
+for x in range(3):
     for y in range(12):
-        file.write(tray + cup + ",  " + sampletext[x][y] + "\n")
+        file.write(tray1 + cup + ",  " + sampletext[x][y] + "\n")
         cupnumber = cupnumber + 1
         cup = '{0:05d}'.format(cupnumber)
-
-file.close()
+for x in range(3, 6):
+    for y in range(12):
+        file.write(tray2 + cup + ",  " + sampletext[x][y] + "\n")
+        cupnumber = cupnumber + 1
+        cup = '{0:05d}'.format(cupnumber)
+for x in range (6, 9):
+    for y in range(12):
+        file.write(tray3 + cup + ",  " + sampletext[x][y] + "\n")
+        cupnumber = cupnumber + 1
+        cup = '{0:05d}'.format(cupnumber)
+for x in range(9, 12):
+    for y in range(12):
+        file.write(tray4 + cup + ",  " + sampletext[x][y] + "\n")
+        cupnumber = cupnumber + 1
+        cup = '{0:05d}'.format(cupnumber)
+for x in range (12, 15):
+    for y in range(12):
+        file.write(tray5 + cup + ",  " + sampletext[x][y] + "\n")
+        cupnumber = cupnumber + 1
+        cup = '{0:05d}'.format(cupnumber)
