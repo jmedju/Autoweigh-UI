fba86541c342b328021fa67019d3ad94443f2568
mtkelly699 <matthew.kelly699@gmail.com>
2021-09-02 14:39:26 -0400

    Update Data_Output_Test.py

--- Data_Output_Test.py
+++ Data_Output_Test.py
@@ -6,6 +6,7 @@ import random
 import threading
 import time
 import datetime
+from datetime import datetime
 
 samples = [[0 for x in range(12)] for x in range(15)]
 sampletext = [[0 for x in range(12)] for x in range(15)]
@@ -13,11 +14,14 @@ sampletext = [[0 for x in range(12)] for x in range(15)]
 
 for x in range(15):
     for y in range(12):
-        self.samples[x][y] = self.tray.create_oval((200 * math.floor(x/3)) + (55 * (x % 3)) + 5, 55 * y + 5, (200 * math.floor(x/3)) + (55 * (x % 3)) + 55, 55 * y + 55, fill = "gray")
-        self.sampletext[x][y] = self.tray.create_text((200 * math.floor(x/3)) + (55 * (x % 3)) + 30, 55 * y + 30, text = "0", justify = tk.CENTER, font = "Helvetica 16")
+        #self.samples[x][y] = self.tray.create_oval((200 * math.floor(x/3)) + (55 * (x % 3)) + 5, 55 * y + 5, (200 * math.floor(x/3)) + (55 * (x % 3)) + 55, 55 * y + 55, fill = "gray")
+        sampletext[x][y] = (200 * math.floor(x/3)) + (55 * (x % 3)) + 30, 55 * y + 30
 
 now = datetime.now()
 
- print(now)
+print(now)
+dt = now.strftime("%y_%m_%d Time_%Hh%Mm%Ss")
+
+print(dt)
 
 #filename =
