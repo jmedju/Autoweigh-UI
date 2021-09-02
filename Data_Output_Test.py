8587e81df8571a350f242f86bf36aff4b13dbe08
mtkelly699 <matthew.kelly699@gmail.com>
2021-09-02 14:45:34 -0400

    Update Data_Output_Test.py

--- Data_Output_Test.py
+++ Data_Output_Test.py
@@ -8,14 +8,12 @@ import time
 import datetime
 from datetime import datetime
 
-samples = [[0 for x in range(12)] for x in range(15)]
-sampletext = [[0 for x in range(12)] for x in range(15)]
+sampletext = [[0 for x in range(12)] for x in range(15)] #12 by 15 matrix from the UI code
 
 
 for x in range(15):
     for y in range(12):
-        #self.samples[x][y] = self.tray.create_oval((200 * math.floor(x/3)) + (55 * (x % 3)) + 5, 55 * y + 5, (200 * math.floor(x/3)) + (55 * (x % 3)) + 55, 55 * y + 55, fill = "gray")
-        sampletext[x][y] = (200 * math.floor(x/3)) + (55 * (x % 3)) + 30, 55 * y + 30
+        sampletext[x][y] = round(random.gauss(50, 5), 2) #Fills matrix with random dummy data
 
 now = datetime.now()
 
@@ -24,4 +22,6 @@ dt = now.strftime("%y_%m_%d Time_%Hh%Mm%Ss")
 
 print(dt)
 
+
+print(sampletext[0][1])
 #filename =
